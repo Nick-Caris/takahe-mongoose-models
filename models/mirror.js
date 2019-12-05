95c69392562275b5de070a31f3ced8b4b5e085fa
Nick Caris <nickcaris97@gmail.com>
2019-12-05 13:08:06 +0100

    added user to mirrror

--- models/mirror.js
+++ models/mirror.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
+const userSchema = require('./user');
+
 const mirrorSchema = mongoose.Schema({
   deviceId: {type: String, required: true},
   description: {type: String, required: true},
+  users: [userSchema],
 });
 
 const Mirror = mongoose.model('Mirror', mirrorSchema);
