97d56f559cd2224590b39536f1b503fd80e04cdb
Nick Caris <nickcaris97@gmail.com>
2019-12-05 13:05:51 +0100

    name not unique

--- models/file.js
+++ models/file.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const fileSchema = mongoose.Schema({
-  name: {type: String, unique: true, required: true},
+  name: {type: String, required: true},
   contentType: {type: String, required: true},
   data: Buffer,
 });
