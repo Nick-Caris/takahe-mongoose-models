8b21d3b2d0c8bf74f1d6f7d13a5f345b2d1d7c55
Nick Caris <nickcaris97@gmail.com>
2019-12-05 10:37:46 +0100

    made the requires o the same tab

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Imports
-const file = require('./models/file.js');
-const user = require('./models/user.js');
-const widget = require('./models/widget.js');
-const userWidget = require('./models/userWidget.js');
-const mirror = require('./models/mirror.js');
+const file =        require('./models/file.js');
+const user =        require('./models/user.js');
+const widget =      require('./models/widget.js');
+const userWidget =  require('./models/userWidget.js');
+const mirror =      require('./models/mirror.js');
 
 //Exports
 module.exports = {file, user, widget, userWidget, mirror};
