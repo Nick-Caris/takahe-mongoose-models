53a1a95e32a3b803ea0a322807902647be12dd90
Nick Caris <nickcaris97@gmail.com>
2019-12-11 12:20:39 +0100

    changed all the relationships and added icon

--- models/mirror.js
+++ models/mirror.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema = require('./user');
-
 const mirrorSchema = mongoose.Schema({
   deviceId: {type: String, required: true},
   description: {type: String, required: true},
-  users: [userSchema],
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
 });
 
 const Mirror = mongoose.model('Mirror', mirrorSchema);
--- models/userWidget.js
+++ models/userWidget.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const widgetSchema = require('./widget');
 
 const userWidgetSchema = mongoose.Schema({
-  widget: {type: widgetSchema, required: true},
-  position: {type: Number, required: true, min: 0, max: 5},
+  widget: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Widget',
+    required: true
+  },
   active: {type: Boolean, required: true},
   userData: Object,
 });
