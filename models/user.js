6d068ee774600f3b54e609033031b4fd25db5a91
Tim <t.hemmes@student.han.nl>
2019-12-12 09:51:48 +0100

    change userwidget in user from ref to embedded

--- models/user.js
+++ models/user.js
@@ -3,17 +3,13 @@ const mongoose = require('mongoose');
 const fileSchema = require('./file');
 const userWidgetSchema = require('./userWidget');
 
-
 const userSchema = mongoose.Schema({
   name: {type: String, required: true},
   images: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'File'
   }],
-  widgets: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'UserWidget'
-  }],
+  widgets: [userWidgetSchema],
 });
 
 mongoose.model('User', userSchema);
